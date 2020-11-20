ee651197c076d1f04c7303a34201ff822e3f49aa
VLAD-KHARCHENKO <vladuslav.kharchenko.jd@gmail.com>
2020-11-21 01:05:26 +0200

    jspCommit

--- src/main/java/servlets/MainServlet.java
+++ src/main/java/servlets/MainServlet.java
@@ -1,58 +1,40 @@
 package servlets;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns = {"/cool-servlet"})
+
+@WebServlet(urlPatterns = {"/cool-servlet", "/my-cool-servlet/*"})
 public class MainServlet extends HttpServlet {
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-        log("Method init =)");
-    }
+    private double[] Arr = new double[6];
+    Service service = new Service();
+
 
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Method service enter\n");
-        super.service(req, resp);
-        resp.getWriter().write("Method service exit\n");
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        String uri = req.getRequestURI();
-        String params = formatParams(req);
-
-        resp.getWriter().write("Method doGet\nURI: " + uri + "\nParams:\n" + params + "\n");
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("notification", "result");
+        super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String uri = req.getRequestURI();
-        String params = formatParams(req);
 
-        resp.getWriter().write("Method doPost\nURI: " + uri + "\nParams:\n" + params + "\n");
+        Arr[0] = Double.parseDouble(req.getParameter("a1"));
+        Arr[1] = Double.parseDouble(req.getParameter("b1"));
+        Arr[2] = Double.parseDouble(req.getParameter("c1"));
+        Arr[3] = Double.parseDouble(req.getParameter("a2"));
+        Arr[4] = Double.parseDouble(req.getParameter("b2"));
+        Arr[5] = Double.parseDouble(req.getParameter("c2"));
+
+        String notification = service.gaussian(Arr);
+        req.setAttribute("notification", notification);
+        resp.getWriter().write("Result:\n " + notification + "\n");
+
     }
 
-    private String formatParams(HttpServletRequest req) {
-        return req.getParameterMap()
-                .entrySet()
-                .stream()
-                .map(entry -> {
-                    String param = String.join(" and ", entry.getValue());
-                    return entry.getKey() + " => " + param;
-                })
-                .collect(Collectors.joining("\n"));
-    }
 
-    @Override
-    public void destroy() {
-        log("Method destroy =)");
-    }
 }
---
+++ src/main/webapp/first-jsp.jsp
@@ -0,0 +1,36 @@
+<!doctype html>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri = "http://java.sun.com/jsp/jstl/core" prefix = "c" %>
+<%@ taglib prefix = "fmt" uri = "http://java.sun.com/jsp/jstl/fmt" %>
+<html>
+<head>
+    <title>First JSP</title>
+</head>
+<body>
+<h2>Equation</h2>
+${notification}
+<form method="post" action="/my-app/cool-servlet">
+    <h3>Enter a1:</h3>
+    <input type="text" name="a1">
+    <h3>Enter a2:</h3>
+    <input type="text" name="a2">
+    <h3>Enter b1:</h3>
+    <input type="text" name="b1">
+    <h3>Enter b2:</h3>
+    <input type="text" name="b2">
+    <h3>Enter c1:</h3>
+    <input type="text" name="c1">
+    <h3>Enter c2:</h3>
+    <input type="text" name="c2">
+    <br/>
+    <br/>
+    <button type="submit">Send</button>
+
+</form>
+
+<c:if test="${not empty notification}">
+    <c:out value ="${notification}" />
+</c:if>
+<br/>
+</body>
+</html>
